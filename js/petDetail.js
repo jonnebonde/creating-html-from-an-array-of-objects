a691bd2549ab9add95a03d955863305ef561ec37
jonnebonde <jonnebonde@gmail.com>
2022-01-21 15:38:08 +0100

    enter

--- js/petDetail.js
+++ js/petDetail.js
@@ -1,34 +1,23 @@
 const pet = {
-    type: "pig",
-    colour: "#ff0000",
+    type: "cat",
+    colour: "brown",
     age: 20,
     isFriendly: true,
     name: "Benny",
 };
 
+
 const petDetail = document.querySelector(".pet-detail");
 
 let friendly = "Yes";
 
-// pet.isFriendly === false
-
-if (!pet.isFriendly) {
-    friendly = "No";
-}
-
-const petColour = pet.colour;
-
-let petAge = "Unknown";
-
-// pet.age !== undefined && pet.age !== null && pet.age !== false && pet.age !== 0
-
-if (pet.age) {
-    petAge = pet.age;
-}
+if(pet.isFriendly === false)  {
+    // or if(!pet.isFriendly)//
+    friendly = "no";
+} 
 
 petDetail.innerHTML = `
-<h4 style="color: ${petColour}">${pet.type}</h4>
-<p>Age: ${petAge}</p>
-<p>Friendly: ${friendly}</p>
-<p style="color: ${petColour}">Name: ${pet.name} </p>
-`;
+<h4 style="color: ${pet.colour}">${pet.type}</h4>
+<p>Age: ${pet.age}</p>
+<p>Friendly: ${friendly}</p>`;
+
